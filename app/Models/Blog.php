26dc6a66dcc9aefbba4c3133ee9bcd88097988c0
MohdFaraz <farazahmad280@gmail.com>
2023-11-03 17:12:40 +0530

    Start Mobility solution

---
+++ app/Models/Blog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+  use HasFactory;
+  public function getCategory()
+  {
+    return $this->hasOne(BlogCategory::class, 'id', 'category_id');
+  }
+  public function getAuthor()
+  {
+    return $this->hasOne(Author::class, 'id', 'author_id');
+  }
+}
