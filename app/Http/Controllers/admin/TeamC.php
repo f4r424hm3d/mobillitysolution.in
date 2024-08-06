b5d3a08b2b68fdec89de1cf5b703662107e594bb
MohdFaraz <farazahmad280@gmail.com>
2024-08-06 12:59:10 +0530

    Team Migration

--- app/Http/Controllers/admin/TeamC.php
+++ app/Http/Controllers/admin/TeamC.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class TeamC extends Controller
 {
+  protected $page_route;
   public function __construct()
   {
     $this->page_route = 'team';
---
+++ database/migrations/2024_08_06_072443_add_columns_to_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->string('department')->nullable()->after('profile_picture');
+      $table->string('branch')->nullable()->after('department');
+      $table->string('experience_short')->nullable()->after('branch');
+      $table->text('languages')->nullable()->after('experience_short');
+      $table->longText('highlights')->nullable()->after('languages');
+      $table->longText('experiance')->nullable()->after('highlights');
+      $table->longText('education')->nullable()->after('experiance');
+      $table->longText('shortnote')->nullable()->after('education');
+      $table->unsignedBigInteger('working_status')->nullable()->after('education');
+      $table->foreign('working_status')->references('id')->on('employee_statuses')->nullOnDelete();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropForeign(['working_status']);
+      $table->dropColumn([
+        'department',
+        'branch',
+        'experience_short',
+        'languages',
+        'highlights',
+        'experiance',
+        'education',
+        'shortnote',
+        'working_status',
+      ]);
+    });
+  }
+};
